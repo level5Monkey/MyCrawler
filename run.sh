8598d692f24bfb688bd1eb3fc0cce7361f2adde7
Jiaxing Li <c0906henry@163.com>
2021-12-23 11:41:07 -0500

    made run.sh get current path

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+BASEDIR=$(dirname "$0")
 while IFS= read -r keyword; do
     /bin/scrapy crawl AmazonCrawler -a keyword=$keyword 2>&1 | tee -a AmazonCrawler.log
     sleep $(( $RANDOM % 50 + 1))
-done < /home/c0906/AmazonCrawler/keywords
+done < $BASEDIR/keywords
